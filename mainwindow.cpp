7a6e51358fea0389135959c902e0d7e08ab98b5a
redmh <redmh514@163.com>
2024-08-13 18:37:27 +0800

    更新GitHub链接。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -488,7 +488,7 @@ void MainWindow::on_PB_batterypause_toggled(bool checked)
 
 void MainWindow::on_PB_github_clicked()
 {
-    const QUrl url("https://github.com/AkntRedMH/VideoWallpaper");
+    const QUrl url("https://github.com/redmh-akamk/VideoWallpaper");
     QDesktopServices::openUrl(url);
     this->hide();
 }
